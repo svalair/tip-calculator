d084a5eb9383f7d8125dd552013978638fe98738
amyjzhu <a.zhu@alumni.ubc.ca>
2017-07-18 11:30:01 -0700

    Made some changes to system to include rounding for .within, .above, etc. testability

--- src/TipCalculator.ts
+++ src/TipCalculator.ts
@@ -2,28 +2,66 @@
  * Created by sazivalair on 7/13/17.
  */
 
+export enum RoundingStyle {
+    UP, DOWN, DEFAULT, NONE
+}
+
+
 export class TipCalculator {
 
-    constructor() {
+
+    bill : number; // in dollars
+    guests : number;
+    rounding : RoundingStyle;
+    tip : number; // in percent
+
+
+
+    constructor(bill : number, guests : number, tip : number, rounding = RoundingStyle.DEFAULT) {
+        this.bill = bill;
+        this.guests = guests;
+        this.tip = tip;
+        this.rounding = rounding;
     }
 
-    public static calculateTip(bill: number, percent: number): number {
-        return bill * percent;
+
+
+    public calculateTotalTip() : number {
+        return this.bill * this.tip;
     }
 
-    public divideBill(bill: number, guests: number): number {
-        return bill / guests;
+    public splitBill() : number {
+        return this.bill / this.guests;
     }
 
-    public divideTip(tip: number, guests: number): number {
-        return tip / guests;
+    public divideTip(): number {
+        return this.calculateTotalTip() / this.guests;
     }
 
-    public totalPerGuest(bill: number, percent: number, guests: number): number {
-        let tip = TipCalculator.calculateTip(bill, percent);
-        let tipPerGuest = this.divideTip(tip, guests);
-        let billPerGuest = this.divideBill(bill, guests);
-        return tipPerGuest + billPerGuest;
+    public totalPerGuest() : number {
+        let tipPerGuest = this.divideTip();
+        let billPerGuest = this.splitBill();
+        return this.round(tipPerGuest + billPerGuest);
+    }
+
+
+    private round(num : number) : number {
+        switch (this.rounding) {
+            case (RoundingStyle.UP):
+                return Math.ceil(num);
+            case (RoundingStyle.DOWN):
+                return Math.floor(num);
+            case (RoundingStyle.NONE):
+                return num;
+            case (RoundingStyle.DEFAULT):
+            default:
+                if (num - Math.floor(num) >= 0.5) {
+                    return Math.ceil(num);
+                } else {
+                    return Math.floor(num);
+                }
+
+        }
     }
 
 }
--- test/TipCalculator.test.ts
+++ test/TipCalculator.test.ts
@@ -4,7 +4,7 @@
 
 import {expect} from 'chai';
 import 'mocha';
-import {TipCalculator} from "../src/TipCalculator";
+import {TipCalculator, RoundingStyle} from "../src/TipCalculator";
 
 
 describe("Basic test for TipCalculator", function () {
@@ -12,17 +12,18 @@ describe("Basic test for TipCalculator", function () {
     let t: TipCalculator;
 
     beforeEach(function () {
-        t = new TipCalculator();
+
     });
 
     afterEach(function () {
     });
 
     it("Should calculate the correct tip percentage", function () {
-
         let bill = 20;
         let percent = 0.15;
-        let tip = TipCalculator.calculateTip(bill, percent);
+
+        let t = new TipCalculator(bill,1,percent);
+        let tip = t.calculateTotalTip();
         expect(tip).to.equal(20 * 0.15);
 
     });
@@ -31,27 +32,61 @@ describe("Basic test for TipCalculator", function () {
 
         let bill = 10;
         let guests = 5;
-        let billPerGuest = t.divideBill(bill, guests);
+        let t = new TipCalculator(bill,guests,0);
+        let billPerGuest = t.splitBill();
         expect(billPerGuest).to.equal(bill / guests);
 
     });
 
     it("Should divide the tip evenly between guests", function () {
 
-        let tip = 25;
+        let bill = 25;
+        let tip = 0.20;
         let guests = 2;
-        let tipPerGuest = t.divideTip(tip, guests);
-        expect(tipPerGuest).to.equal(tip / guests);
+        let t = new TipCalculator(bill,guests,tip);
+
+        let tipPerGuest = t.divideTip();
+
+        expect(tipPerGuest).to.equal(bill * tip / guests);
 
     });
 
-    it("Should calculate the total bill and tip per guest", function () {
+    it("Should calculate the total bill and tip per guest for no rounding", function () {
         let guests = 3;
         let bill = 87;
         let percent = 0.20;
-        let totalPerGuest = t.totalPerGuest(bill, percent, guests);
+        let t = new TipCalculator(bill,guests,percent, RoundingStyle.NONE);
+        let totalPerGuest = t.totalPerGuest();
         let expectedTotalPerGuest = (87 / 3) + ((87 * 0.20) / 3);
         expect(totalPerGuest).to.equal(expectedTotalPerGuest);
+    });
+
+    it("Should calculate the total bill and tip per guest for rounding up", function () {
+        let guests = 3;
+        let bill = 87;
+        let percent = 0.20;
+        let t = new TipCalculator(bill,guests,percent, RoundingStyle.UP);
+        let totalPerGuest = t.totalPerGuest();
+
+        expect(totalPerGuest).to.be.above(bill/guests);
+        expect(totalPerGuest).to.be.above(bill*(1+percent)/guests);
+
+        expect(totalPerGuest*guests).to.be.within(bill*(1+percent),bill*(1+percent)+1);
+
+    });
+
+    it("Should calculate the total bill and tip per guest for rounding down", function () {
+        let guests = 3;
+        let bill = 87;
+        let percent = 0.20;
+        let t = new TipCalculator(bill,guests,percent, RoundingStyle.DOWN);
+        let totalPerGuest = t.totalPerGuest();
+
+        expect(totalPerGuest).to.be.above(bill/guests);
+        expect(totalPerGuest*guests).to.be.below(bill*(1+percent));
+
+        expect(totalPerGuest*guests).to.be.closeTo(bill,bill*percent);
+
     });
 
 });
